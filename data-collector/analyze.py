1f271c6fd4dcb58f6f9c9cd08e9627eece471fb1
Florrdv <flor@ronsmans.me>
2022-05-12 16:52:07 +0200

    Added await support to analyzer

--- data-collector/analyze.py
+++ data-collector/analyze.py
@@ -6,8 +6,8 @@ import json
 Keywords overview:
 ✅ False
 ✅ True
-⛔️ None
-⛔️ await
+✅ None
+✅ await
 ⛔️ else
 ⛔️ import
 ⛔️ pass
@@ -70,19 +70,24 @@ class Analyzer(ast.NodeVisitor):
 
     def visit_Constant(self, node):
         # Handle True and False
-        print(type(node.value))
         if type(node.value) == bool:
             self.count_keyword(str(node.value))
-            
+
         # Handle None
         elif type(node.value) == type(None):
             self.count_keyword(str(node.value))
             
         self.generic_visit(node)
 
+    def visit_Await(self, node):
+        # Handle await 
+        self.count_keyword("await")
+
+        self.generic_visit(node)
+
     def generic_visit(self, node):
-        # print(type(node).__name__)
-        # print(type(node).__name__)
+        print(type(node).__name__)
+        print(type(node).__name__)
         ast.NodeVisitor.generic_visit(self, node)
 
 def main():
