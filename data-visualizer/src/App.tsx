a2fc7b13a15207cce8e3b75fb8827aee1e62aeaf
Florrdv <flor@ronsmans.me>
2022-05-12 20:17:35 +0200

    Added python icon to toolbar, added favicon, changed page title

--- data-visualizer/src/App.tsx
+++ data-visualizer/src/App.tsx
@@ -1,18 +1,5 @@
 import Editor from "./Editor"
 
-const Header = ({ fileName }: { fileName: string }) => {
-    return <>
-        <div className="bg-neutral-900 p-2 pb-0 flex flex-row justify-between items-center">
-            <div className="bg-neutral-600 p-1 pl-2 pr-2 cursor-pointer">{fileName}</div>
-            <div className="flex flex-row gap-1 items-center justify-center pb-2">
-                <div className="rounded-full w-4 h-4 bg-red-500 cursor-pointer" />
-                <div className="rounded-full w-4 h-4 bg-orange-500 cursor-pointer" />
-                <div className="rounded-full w-4 h-4 bg-green-500 cursor-pointer" />
-            </div>
-        </div>
-    </>
-}
-
 export type Bounds = number[]
 type HexColor = string
 
@@ -33,6 +20,22 @@ export interface Theme {
     keywords: HexColor,
 }
 
+const Header = ({ fileName }: { fileName: string }) => {
+    return <>
+        <div className="bg-neutral-900 p-2 pb-0 flex flex-row justify-between items-center">
+            <div className="bg-neutral-600 p-1 pl-2 pr-2 cursor-pointer flex items-center justify-center gap-1 rounded-t-md">
+                <img src="./python.png" alt="Python file" className="w-3 h-3" />
+                {fileName}
+            </div>
+            <div className="flex flex-row gap-1 items-center justify-center pb-2">
+                <div className="rounded-full w-4 h-4 bg-red-500 cursor-pointer" />
+                <div className="rounded-full w-4 h-4 bg-orange-500 cursor-pointer" />
+                <div className="rounded-full w-4 h-4 bg-green-500 cursor-pointer" />
+            </div>
+        </div>
+    </>
+}
+
 const App = () => {
     const highlightSpecs: HighlightSpecs = {
         "code": "from string import ascii_lowercase\n\ndef main():\n    # This is a comment!\n    message = \"Hello, alphabet!\"\n    print(message, ascii_lowercase)\n\nmain()",
@@ -50,7 +53,6 @@ const App = () => {
             ]
         }
     }
-    // dodododoIII
 
     const themes: Theme[] = [
         {
