fbbc2d5f35977a62425a272f99d64728ae3b353a
Florrdv <flor@ronsmans.me>
2022-05-12 18:44:00 +0200

    Changed pointer on header

--- data-visualizer/src/App.tsx
+++ data-visualizer/src/App.tsx
@@ -5,11 +5,11 @@ const Header = () => {
     
     return <>
         <div className="bg-neutral-900 p-2 pb-0 flex flex-row justify-between items-center">
-            <div className="bg-neutral-600 p-1 pl-2 pr-2">{fileName}</div>
+            <div className="bg-neutral-600 p-1 pl-2 pr-2 cursor-pointer">{fileName}</div>
             <div className="flex flex-row gap-1 items-center justify-center pb-2">
-                <div className="rounded-full w-4 h-4 bg-red-500" />
-                <div className="rounded-full w-4 h-4 bg-orange-500" />
-                <div className="rounded-full w-4 h-4 bg-green-500" />
+                <div className="rounded-full w-4 h-4 bg-red-500 cursor-pointer" />
+                <div className="rounded-full w-4 h-4 bg-orange-500 cursor-pointer" />
+                <div className="rounded-full w-4 h-4 bg-green-500 cursor-pointer" />
             </div>
         </div>
     </>
