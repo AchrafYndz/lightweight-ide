5be9b07d62ed59a301478697f7e01737528efae3
michel <michel.dierckx@student.uantwerpen.be>
2022-05-15 21:40:35 +0200

    added support for single line comments

--- core/src/other/python_to_enfa.cpp
+++ core/src/other/python_to_enfa.cpp
@@ -47,3 +47,77 @@ void python_to_enfa::scan_to_file(std::ostream &out, const std::string& file) co
     }
 
 }
+std::map<std::pair<std::pair<int, int>, std::pair<int, int>>, std::string> python_to_enfa::split_text(
+  const std::string &file) const {
+
+    std::map<std::pair<std::pair<int, int>, std::pair<int, int>>, std::string> result;
+    std::vector<std::pair<char,std::pair<int,int>>> chars = scan(file);
+
+    // SINGLE LINE COMMENTS
+    int index = 0;
+    std::vector<std::pair<char,std::pair<int,int>>>::iterator it;
+    for (it = chars.begin(); it !=chars.end(); it++){
+        if (it->first == '#'){
+            int row = it->second.second;
+            int column = it->second.first;
+
+            int quote_double_count = 0;
+            int quote_single_count = 0;
+            bool quote_double_matched = true;
+            bool quote_single_matched = true;
+            std::vector<std::pair<char,std::pair<int,int>>>::iterator it2;
+            for (it2 = chars.begin(); it2 !=chars.end(); it2++){
+                if (it2->second.second == row and it2->second.first < column and it2->first == '"'){
+                    if (quote_single_matched){
+                        quote_double_count++;
+                    }
+                    if (quote_double_count%2 != 0){
+                        quote_double_matched = false;
+                    } else {
+                        quote_double_matched = true;
+                    }
+                }
+                if (it2->second.second == row and it2->second.first < column and it2->first == '\''){
+                    if (quote_double_matched){
+                        quote_single_count++;
+                    }
+                    if (quote_single_count%2 != 0){
+                        quote_single_matched = false;
+                    } else{
+                        quote_single_matched = true;
+                    }
+                }
+
+            }
+
+            if (quote_double_matched and quote_single_matched){
+                std::string comment;
+
+
+                std::vector<std::pair<char,std::pair<int,int>>>::iterator it3;
+                for (it3 = chars.begin(); it3 !=chars.end(); it3++){
+                    if(it3->second.second == row and it3->second.first >= column and it3->first != '\n'){
+                        comment += it3->first;
+                    }
+                }
+                int str_length = (int) comment.length();
+                result[std::make_pair(std::make_pair(it->second.first, it->second.second),std::make_pair(it->second.first, it->second.second + str_length))] = comment;
+            }
+        }
+        index++;
+    }
+    return result;
+}
+
+void python_to_enfa::print_map(std::map<std::pair<std::pair<int, int>, std::pair<int, int>>, std::string> &m,
+                               std::ostream &out) const {
+    std::map<std::pair<std::pair<int, int>, std::pair<int, int>>, std::string>::iterator it;
+    for (it = m.begin(); it != m.end(); it++){
+        int column_1 =  it->first.first.first;
+        int row_1 = it->first.first.second;
+        int column_2 =  it->first.second.first;
+        int row_2 = it->first.second.second;
+        std::string str = it->second;
+        out << "(" << column_1 <<", " << row_1 << ")" << " " << "(" << column_2 <<", " << row_2 << ")" << " " << str << std::endl;
+    }
+}
--- core/src/other/python_to_enfa.h
+++ core/src/other/python_to_enfa.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <map>
 
 class python_to_enfa {
   public:
@@ -13,6 +14,9 @@ class python_to_enfa {
     // reads all characters from file, store them in vector with row and column number
     std::vector<std::pair<char,std::pair<int,int>>> scan(const std::string& file) const;
     void scan_to_file(std::ostream &out,const std::string& file) const;
+    // splits text in potential tokens
+    std::map< std::pair<std::pair<int,int>, std::pair<int,int>> , std::string> split_text(const std::string& file) const;
+    void print_map(std::map< std::pair<std::pair<int,int>, std::pair<int,int>> , std::string>& m, std::ostream& out) const;
 
 };
 
