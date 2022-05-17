8b46abcbed9a7398d1c1e9bffb31e65926a82e96
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-05-17 21:48:49 +0200

    added basic simulation support for PDFA's

---
+++ core/src/automata/PDFA.h
@@ -0,0 +1,31 @@
+#ifndef IDE_PDFA_H
+#define IDE_PDFA_H
+
+#include "FA.h"
+
+#include "../../lib/nlohmann-json/json.hpp"
+
+#include <cstdlib>
+#include <ctime>
+
+class PDFA : public FA {
+    // Pair of a Transiton and a chance
+    std::map<const Transition *, double> chances;
+
+    State *currentState = nullptr;
+    bool stuck = false;
+
+  public:
+    PDFA() = default;
+    PDFA(const std::string &file);
+    PDFA(const std::vector<char> alphabet, const std::vector<State *> &states,
+         const std::vector<Transition *> &transitions, const std::map<Transition *, double> &chances);
+
+    void print(std::ostream &out) const;
+
+    void input(const std::string &in);
+
+    const State *getCurrentState() const;
+};
+
+#endif  // IDE_PDFA_H
