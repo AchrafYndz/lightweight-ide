d8e8b2aeb7b1616b67700c469e0c61ee53826ea3
Florrdv <flor@ronsmans.me>
2022-05-25 11:53:02 +0200

    Outlined basic functions for CLI

--- core/src/other/cli.h
+++ core/src/other/cli.h
@@ -1,6 +1,17 @@
 #ifndef __PROJECTS_LIGHTWEIGHT_IDE_CORE_SRC_OTHER_CLI_H_
 #define __PROJECTS_LIGHTWEIGHT_IDE_CORE_SRC_OTHER_CLI_H_
 
+#include <string>
 
+namespace cli {
+    enum Action {
+        Autocomplete,
+        SyntaxHighlighting
+    };
+
+    Action waitForAction();
+    std::string waitForPath();
+    std::string waitForInput();
+}
 
 #endif // __PROJECTS_LIGHTWEIGHT_IDE_CORE_SRC_OTHER_CLI_H_
