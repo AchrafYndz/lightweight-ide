e6edabeb2b3abe1fb5d2ecf24c29248a8e314fae
AchrafYndz <achraf.yandouzi@gmail.com>
2022-11-19 21:47:34 +0100

    Get CFGTests system ready

---
+++ core/tests/machinesTests/CFGTests.cc
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <sstream>
+#include "../../lib/doctest/doctest.h"
+
+#include "../../lib/path/path.h"
+#include "../../src/machines/CFG.H"
+
+TEST_SUITE("CFGTests") {
+    TEST_CASE("[CFGTests] parseFile0") {
+        const CFG cfg(path::rootDirectory + "/tests/machinesTests/res/input/CFG0.json");
+
+        const std::ifstream expectedFile(path::rootDirectory +
+                                         "/tests/automataTests/res/expected/DFATests-parseFile0.json");
+        std::stringstream expected;
+        expected << expectedFile.rdbuf();
+
+        std::stringstream actual("");
+
+        // std::ofstream tmp(path::rootDirectory + "/tests/automataTests/res/expected/DFATests-parseFile0.json");
+
+        cfg.print(actual);
+
+        CHECK_EQ(expected.str(), actual.str());
+    }
+}
