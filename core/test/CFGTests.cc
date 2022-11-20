0cfabe0fa3ac7bd9736361289241c7ebbdec9d60
AchrafYndz <achraf.yandouzi@gmail.com>
2022-11-20 17:38:31 +0100

    Add first CFG test

--- core/test/CFGTests.cc
+++ core/test/CFGTests.cc
@@ -1,9 +1,25 @@
+#include <fstream>
+#include <sstream>
 #include "doctest/doctest.h"
+#include "path/path.h"
 
-#include "../src/CFG.h"
+#include "../../src/machines/CFG.h"
 
 TEST_SUITE("CFGTests") {
     TEST_CASE("[CFGTests] parseFile0") {
+        CFG cfg(path::rootDirectory + "/tests/machinesTests/res/input/CFG0.json");
 
+        const std::ifstream expectedFile(path::rootDirectory +
+                                         "/tests/machinesTests/res/expected/CFGTests-parseFile0.txt");
+        std::stringstream expected;
+        expected << expectedFile.rdbuf();
+
+        std::stringstream actual("");
+
+        //    std::ofstream tmp(path::rootDirectory + "/tests/machinesTests/res/expected/CFGTests-parseFile0.txt");
+
+        cfg.print(actual);
+
+        CHECK_EQ(expected.str(), actual.str());
     }
 }
