ce484d375d82ed0d8febc389ca95e009cb072851
AchrafYndz <achraf.yandouzi@gmail.com>
2022-11-22 16:39:25 +0100

    Write test for parsing PDA

--- core/test/PDATests.cc
+++ core/test/PDATests.cc
@@ -5,6 +5,19 @@
 #include "automata/PDA.h"
 
 TEST_SUITE("PDATests") {
+    TEST_CASE("[PDATests] parseFile0") {
+        PDA pda("test/res/input/PDA0.json");
+
+        const std::ifstream expectedFile("test/res/expected/PDATests-parseFile0.txt");
+        std::stringstream expected;
+        expected << expectedFile.rdbuf();
+
+        std::stringstream actual("");
+
+        pda.print(actual);
+
+        CHECK_EQ(expected.str(), actual.str());
+    }
     TEST_CASE("[PDATests] PDA2CFG") {
         PDA pda("test/res/input/PDA2CFG0.json");
 
