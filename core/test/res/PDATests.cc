9cbe2b8d217d709afef5bb57aad45918a84d4459
AchrafYndz <achraf.yandouzi@gmail.com>
2022-11-22 16:13:15 +0100

    Write test for PDA2CFG

---
+++ core/test/res/PDATests.cc
@@ -0,0 +1,22 @@
+#include "doctest/doctest.h"
+#include <fstream>
+#include <sstream>
+
+#include "../../src/automata/PDA.h"
+
+TEST_SUITE("PDATests") {
+    TEST_CASE("[PDATests] PDA2CFG") {
+        PDA pda("test/res/input/PDA2CFG0.json");
+
+        const std::ifstream expectedFile("test/res/expected/PDATests-PDA2CFG0.txt");
+        std::stringstream expected;
+        expected << expectedFile.rdbuf();
+
+        std::stringstream actual("");
+
+        CFG cfg = pda.toCFG();
+        cfg.print(actual);
+
+        CHECK_EQ(expected.str(), actual.str());
+    }
+}
