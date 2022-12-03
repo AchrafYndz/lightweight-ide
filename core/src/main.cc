4eee85cfe1cdd4792ed519876a46a117d5b2d128
Florrdv <flor@ronsmans.me>
2022-12-03 15:22:19 +0100

    Added example web server route

--- core/src/main.cc
+++ core/src/main.cc
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "crow.h"
 
+std::string generateJSON() {
+  // TODO: @Flor remove placeholder for prod
+  std::string result = "{ foo: \"bar\" }";
+
+  // TODO: @Achraf syntax designation & error conversion to frontend compatible format
+  // result = ...;
+
+  return result;
+}
+
 int main() {
-    std::cout << "Hello, World!\n";
-    return 0;
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/json")([](){
+
+    return generateJSON();
+  });
+
+  app.port(18080).multithreaded().run();
 }
