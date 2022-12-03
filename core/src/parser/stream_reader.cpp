830ca4223a122559c3d843dadab7bf88f0f292eb
Florrdv <flor@ronsmans.me>
2022-12-03 17:52:58 +0100

    Added sourcePath to streamreader, started working on peek functionality

--- core/src/parser/stream_reader.cpp
+++ core/src/parser/stream_reader.cpp
@@ -1,9 +1,23 @@
 #include "stream_reader.h"
 
-StreamReader::StreamReader(int bufferSize): bufferSize(bufferSize) {}
+StreamReader::StreamReader(std::string sourcePath): sourcePath(sourcePath) {}
 
 char StreamReader::peek(int k) {
-  return 'a';
+  // Let's say we're trying to read the 10th char (k = 10)
+  // and our buffer starts at index 2 (zero-based)
+  // then our relative index is 8
+  int relativeIndex = k - bufferStart;
+
+  if (relativeIndex < 0) {
+    // Reset the buffer the buffer to character k
+    int previousStart = bufferStart;
+    bufferStart = k;
+    // Read k
+
+
+  } else if (relativeIndex >= bufferSize) {
+    // Read to this character
+  } else return buffer[relativeIndex];
 }
 
 char StreamReader::consume(int k) {
--- core/src/parser/stream_reader.h
+++ core/src/parser/stream_reader.h
@@ -1,13 +1,18 @@
 #ifndef __PROJECTS_LIGHTWEIGHT_IDE_V2_CORE_SRC_PARSER_STREAM_READER_H_
 #define __PROJECTS_LIGHTWEIGHT_IDE_V2_CORE_SRC_PARSER_STREAM_READER_H_
 
+#include <string>
+
 class StreamReader {
 private:
   int bufferStart = 0;
-  int bufferSize = 10000;
+  std::string sourcePath;
+
+  const int bufferSize = 10000;
+  char buffer[10000];
 
 public:
-  StreamReader(int bufferSize);
+  StreamReader(std::string sourcePath);
   char peek(int k);
   char consume(int k);
 };
