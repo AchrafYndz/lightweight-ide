8338b443182880778fd8c8a94822ac92f85931f8
Florrdv <flor@ronsmans.me>
2022-12-03 17:33:06 +0100

    Added placeholder code for stream_reader, moved ring_buffer implementation to stream_reader

--- core/src/parser/stream_reader.cpp
+++ core/src/parser/stream_reader.cpp
@@ -1,1 +1,11 @@
 #include "stream_reader.h"
+
+StreamReader::StreamReader(int bufferSize): bufferSize(bufferSize) {}
+
+char StreamReader::peek(int k) {
+  return 'a';
+}
+
+char StreamReader::consume(int k) {
+  return 'a';
+}
--- core/src/parser/stream_reader.h
+++ core/src/parser/stream_reader.h
@@ -2,9 +2,14 @@
 #define __PROJECTS_LIGHTWEIGHT_IDE_V2_CORE_SRC_PARSER_STREAM_READER_H_
 
 class StreamReader {
-  StreamReader() = default;
-  char peek();
-  char consume();
+private:
+  int bufferStart = 0;
+  int bufferSize = 10000;
+
+public:
+  StreamReader(int bufferSize);
+  char peek(int k);
+  char consume(int k);
 };
 
 #endif // __PROJECTS_LIGHTWEIGHT_IDE_V2_CORE_SRC_PARSER_STREAM_READER_H_
