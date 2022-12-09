4850c940a35887a8c4240b1023fb7abe52b4add5
AchrafYndz <achraf.yandouzi@gmail.com>
2022-12-09 19:42:17 +0100

    Reformat all files using ClangFormat

--- core/src/parser/stream_reader.h
+++ core/src/parser/stream_reader.h
@@ -5,22 +5,22 @@
 
 class StreamReader {
 private:
-  std::string sourcePath;
+    std::string sourcePath;
 
-  const int bufferSize = 5;
-  char buffer[5];
+    const int bufferSize = 5;
+    char buffer[5];
 
-  int head = 0;
-  int tail = 0;
-  int bufferStart = 0;
-  int empty = true;
+    int head = 0;
+    int tail = 0;
+    int bufferStart = 0;
+    int empty = true;
 
 public:
-  StreamReader(std::string sourcePath);
-  char peek(int k, int& i);
-  char peek(int k);
-  char consume(int k);
-  int charCount() const;
+    StreamReader(std::string sourcePath);
+    char peek(int k, int& i);
+    char peek(int k);
+    char consume(int k);
+    int charCount() const;
 };
 
 #endif // __PROJECTS_LIGHTWEIGHT_IDE_V2_CORE_SRC_PARSER_STREAM_READER_H_
