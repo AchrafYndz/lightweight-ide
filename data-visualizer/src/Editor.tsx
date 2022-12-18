fca913b697ee3fb3fca379a1878790fd186ec2e9
Florrdv <flor@ronsmans.me>
2022-12-18 17:33:21 +0100

    Added line numbers

--- data-visualizer/src/Editor.tsx
+++ data-visualizer/src/Editor.tsx
@@ -1,5 +1,4 @@
-import { MutableRefObject, RefObject, useEffect, useRef, useState } from "react";
-import { reduceEachLeadingCommentRange } from "typescript";
+import { MutableRefObject, useEffect, useRef } from "react";
 
 type HexColor = string;
 export interface Theme {
@@ -22,210 +21,115 @@ export interface HighlightSpecs {
   bounds: HighlightSpecsBounds;
 }
 
-
-// const Line = ({
-//   highlightSpecsBounds,
-//   theme,
-//   startIndex,
-//   text,
-// }: {
-//   highlightSpecsBounds: HighlightSpecsBounds;
-//   theme: Theme;
-//   startIndex: number;
-//   text: string;
-// }) => {
-//   let n: JSX.Element[] = [];
-//   let content = "";
-//   let currentColor: string | null = null;
-
-//   const pushContent = () => {
-//     if (content.length === 0) return;
-//     n.push(
-//       <span
-//         key={n.length}
-//         style={{ ...(currentColor && { color: currentColor }) }}
-//       >
-//         {content}
-//       </span>
-//     );
-//     content = "";
-//   };
-
-//   for (let i = 0; i < text.length; i++) {
-//     for (const type of Object.keys(highlightSpecsBounds)) {
-//       if (
-//         highlightSpecsBounds[type as keyof HighlightSpecsBounds].find(
-//           (bounds) => bounds[0] === i + startIndex
-//         )
-//       ) {
-//         pushContent();
-//         currentColor = theme[type as keyof Theme];
-//       }
-
-//       if (
-//         highlightSpecsBounds[type as keyof HighlightSpecsBounds].find(
-//           (bounds) => bounds[1] === i + startIndex
-//         )
-//       ) {
-//         pushContent();
-//         currentColor = null;
-//       }
-//     }
-
-//     content += text[i];
-//   }
-
-//   pushContent();
-
-//   return <p className="whitespace-pre">{n}</p>;
-// };
-
-// const convert = ({
-//   bounds,
-//   code,
-// }: {
-//   bounds: HighlightSpecsBoundsRaw;
-//   code: string;
-// }): HighlightSpecsBounds => {
-//   const newBounds = {} as HighlightSpecsBounds;
-//   const lines = code.split("\n");
-
-//   for (const type of Object.keys(bounds)) {
-//     // Let's start off by pre-processing the data
-//     // We have to make sure that all pairs are on the same line before continueing
-//     const boundsOriginal = bounds[type as keyof HighlightSpecsBounds];
-//     const boundsNew = [];
-
-//     for (const pair of boundsOriginal) {
-//       if (pair[0][0] === pair[1][0]) boundsNew.push(pair);
-//       else {
-//         for (let i = pair[0][0]; i <= pair[1][0]; i++) {
-//           // Start
-//           if (i === 0)
-//             boundsNew.push([
-//               [i, pair[0][1]],
-//               [i, lines[i].length - 1],
-//             ]);
-//           // End
-//           else if (i === pair[1][0])
-//             boundsNew.push([
-//               [i, 0],
-//               [i, pair[1][1]],
-//             ]);
-//           // Other
-//           else
-//             boundsNew.push([
-//               [i, 0],
-//               [i, lines[i].length - 1],
-//             ]);
-//         }
-//       }
-//     }
-
-//     // Convert to indexes
-//     for (const coords of boundsNew) {
-//       const start = coords[0];
-//       const end = coords[1];
-
-//       const beginIndex =
-//         lines
-//           .slice(0, start[0])
-//           .reduce((total, line) => total + line.length, 0) +
-//         start[0] * "\n".length +
-//         start[1];
-//       const endIndex =
-//         lines.slice(0, end[0]).reduce((total, line) => total + line.length, 0) +
-//         end[0] * "\n".length +
-//         end[1] +
-//         1;
-
-//       newBounds[type as keyof HighlightSpecsBounds] ??= [];
-//       newBounds[type as keyof HighlightSpecsBounds].push([
-//         beginIndex,
-//         endIndex,
-//       ]);
-//     }
-//   }
-
-//   return newBounds;
-// };
-
 const highlight = async (code: string): Promise<HighlightSpecs> => {
   const response = await fetch("http://0.0.0.0:18080/json", {
     method: "POST",
     headers: {
-      "content-type": "text/plain"
+      "content-type": "text/plain",
     },
-    body: code
-  })
+    body: code,
+  });
 
-  return response.json()
-}
+  return response.json();
+};
 
-const f = async (content: string, ref: MutableRefObject<HTMLDivElement>, theme: Theme) => {
-  // console.log(e)
-    console.log("hey")
-    // Prevent the Save dialog to open
-    // Place your code here
-    // setHighlightSpecs(await highlight(code))
-    const s = await highlight(content)
-    // console.log(s)
-    let html = content
-    console.log(html)
-    
-    const merged = []
-    for (const key of Object.keys(s.bounds)) merged.push(...(s.bounds[key as keyof HighlightSpecsBounds].map(c => ({bounds: c, color: theme[key as keyof Theme]}))))
-    merged.sort((a, b) => a.bounds[0] - b.bounds[0])
-    
-    let offset = 0
-    for (const pair of merged) {
-      pair.bounds[0] += offset
-      pair.bounds[1] += offset
-      const original = html.substring(pair.bounds[0], pair.bounds[1])
-      const target = `<span&nbspp;style="color:${pair.color};">${original}</span>`
-      // const target = original
-      // const target = html.substring(pair.bounds[0], pair.bounds[1])
-      html = html.substring(0, pair.bounds[0]) + target + html.substring(pair.bounds[1], html.length) 
+const f = async (
+  content: string,
+  ref: MutableRefObject<HTMLDivElement>,
+  theme: Theme
+) => {
 
-      offset += target.length - original.length
-    }
+  const spec = await highlight(content);
 
-    // console.log(html)
+  const merged = [];
+  for (const key of Object.keys(spec.bounds))
+    merged.push(
+      ...spec.bounds[key as keyof HighlightSpecsBounds].map((c) => ({
+        bounds: c,
+        color: theme[key as keyof Theme],
+      }))
+    );
+  merged.sort((a, b) => a.bounds[0] - b.bounds[0]);
 
-    html = html.replace(/ /g, "&nbsp;")
-    html = html.replace(/&nbspp;/g, " ")
-    html = html.replace(/\n/g, "<br>")
-    // console.log(html === ref.current.innerText)
+  let html = content;
+  let offset = 0;
+  for (const pair of merged) {
+    pair.bounds[0] += offset;
+    pair.bounds[1] += offset;
+    const original = html.substring(pair.bounds[0], pair.bounds[1]);
+    const target = `<span&nbspp;style="color:${pair.color};">${original}</span>`;
+    html =
+      html.substring(0, pair.bounds[0]) +
+      target +
+      html.substring(pair.bounds[1], html.length);
 
-    // console.log(html)
-    // ref.current.innerHTML = html
-    ref.current.innerHTML = html
-    console.log(ref.current.innerHTML)
+    offset += target.length - original.length;
+  }
 
+  html = html.replace(/ /g, "&nbsp;");
+  html = html.replace(/&nbspp;/g, " ");
+  html = html.replace(/\n/g, "<br>");
 
-    // ref.current.innerHTML = ref.current.innerHTML.replace("banana", "<span style=\"background: red;\">banana</span>")
-}
+  ref.current.innerHTML = html;
+};
+
+// Source (modified): https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea
+const tabHandler = (e: KeyboardEvent, textarea: HTMLTextAreaElement) => {
+  if (e.key !== "Tab") return;
+  e.preventDefault();
+
+  // Get cursor location
+  const start = textarea.selectionStart;
+  const end = textarea.selectionEnd;
+
+  // Insert tab
+  textarea.value =
+    textarea.value.substring(0, start) + "    " + textarea.value.substring(end);
+
+  textarea.selectionStart = textarea.selectionEnd = start + 4;
+};
 
 const Editor = () => {
   const theme: Theme = {
-      strings: "#97c279",
-      comments: "#7b808a",
-      keywords: "#c678dd",
-    }
-  const ref = useRef() as MutableRefObject<HTMLDivElement>;
-  
+    strings: "#97c279",
+    comments: "#7b808a",
+    keywords: "#c678dd",
+  };
+
+  const mirrorRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const inputRef = useRef() as MutableRefObject<HTMLTextAreaElement>;
+
+  useEffect(() => {
+    if (!inputRef.current) return;
+    const current = inputRef.current;
+    const tabHandlerCurrent = (e: KeyboardEvent) => tabHandler(e, current);
+
+    current.addEventListener("keydown", tabHandlerCurrent);
+
+    return () => {
+      current.removeEventListener("keydown", tabHandlerCurrent);
+    };
+  }, [inputRef]);
+
   return (
     <>
-      <div className="flex flex-col w-full h-full bg-neutral-800 p-5 overflow-scroll relative">
-          <div ref={ref}
-          className="w-full h-full bg-neutral-800 focus:outline-none fixed">
-            
-          </div>
+      <div className="flex flex-row w-full bg-neutral-800 p-5 gap-2">
+        <div className="flex flex-col text-gray-400">
+          {[...Array(100).keys()].map((t) => (
+            <div className="w-2 flex items-end justify-end">{t}</div>
+          ))}
+        </div>
+        <div className="flex w-full h-full relative">
           <textarea
-            // ref={ref}
-            onInput={e => f(e.currentTarget.value, ref, theme)}
-            className="w-full h-full bg-neutral-800 focus:outline-none z-10 text-transparent bg-transparent caret-white"
-          ></textarea>
+              ref={inputRef}
+              onInput={(e) => f(e.currentTarget.value, mirrorRef, theme)}
+              className="w-full h-full bg-neutral-800 focus:outline-none z-10 text-transparent bg-transparent caret-white"
+            />
+          <div
+            ref={mirrorRef}
+            className="w-full h-full bg-neutral-800 absolute"
+          />
+        </div>
       </div>
     </>
   );
