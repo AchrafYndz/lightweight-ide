2cc6a5f35e1bb94e0daa7e8750c341db3553bd1c
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-12-23 17:42:24 +0100

    added `Scanner` class for `Lexer`

---
+++ core/src/parser/scanner.cc
@@ -0,0 +1,35 @@
+#include "scanner.h"
+
+std::tuple<unsigned int, unsigned int, char> Scanner::get_next_char() {
+    // TODO: make this better
+    char next_char;
+    try {
+        next_char = this->reader.consume(this->index);
+    } catch (EOFException&) {
+        next_char = '\0';
+    }
+    const auto result = std::make_tuple(this->row, this->col, next_char);
+
+    // increment counters
+    if (next_char == '\n') {
+        ++this->row;
+        this->col = 0;
+    } else {
+        ++this->col;
+    }
+
+    ++this->index;
+
+    return result;
+}
+
+std::tuple<unsigned int, unsigned int, char> Scanner::peek_next_char() {
+    // TODO: make this better
+    char next_char;
+    try {
+        next_char = this->reader.peek(this->index);
+    } catch (EOFException&) {
+        next_char = '\0';
+    }
+    return std::make_tuple(this->row, this->col, next_char);
+}
---
+++ core/src/parser/scanner.h
@@ -0,0 +1,31 @@
+#ifndef IDE_SRC_PARSER_SCANNER_H
+#define IDE_SRC_PARSER_SCANNER_H
+
+#include "stream_reader.h"
+
+#include <tuple>
+
+class Scanner {
+public:
+    using NextChar = std::tuple<unsigned int, unsigned int, char>;
+
+public:
+    inline Scanner(StreamReader reader) : reader(reader) {}
+
+    /// Returns `{row, col, character}` of the next character.
+    /// Note: Both `row` and `col` use zero-based indexing.
+    NextChar get_next_char();
+
+    /// Returns `{row, col, character}` of the next character without advancing the internal buffer and counters.
+    /// Note: Both `row` and `col` use zero-based indexing.
+    /// TODO: cannot be const because of implementation of `StreamReader::peek`
+    NextChar peek_next_char();
+
+private:
+    StreamReader reader;
+
+    unsigned int row{0}, col{0};
+    unsigned int index{0};
+};
+
+#endif // IDE_SRC_PARSER_SCANNER_H
