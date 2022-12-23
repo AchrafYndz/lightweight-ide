c567af5d250bf848b12a9b026db15b32fcd86fb6
exellentcoin26 <jonas.caluwe26@gmail.com>
2022-12-23 20:42:10 +0100

    added tests for `Lexer`

--- core/test/LexerTests.cc
+++ core/test/LexerTests.cc
@@ -7,7 +7,7 @@
 #include <iostream>
 #include <vector>
 
-/// help function to format `{row, col, character}` vector of tuples
+/// Helper function to format `{row, col, character}` vector of tuples.
 std::ostream& operator<<(std::ostream& out, const std::vector<Scanner::NextChar>& v) {
     out << '{';
 
@@ -17,6 +17,37 @@ std::ostream& operator<<(std::ostream& out, const std::vector<Scanner::NextChar>
         out << '`' << std::get<0>(*it) << "`,`" << std::get<1>(*it) << "`,`" << std::get<2>(*it) << '`';
 
         out << ((std::next(it) == v.end()) ? ")" : "),");
+    }
+
+    out << "}\n";
+
+    return out;
+}
+
+/// helper function to format `{token_type, {{row_start, col_start}, {row_end, col_end}, token}}`
+std::ostream& operator<<(std::ostream& out, const std::vector<Lexer::NextToken>& v) {
+    out << '{';
+
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        out << '{';
+
+        // out << (std::vector<const char*>{"Comment", "Keyword", "Literal", "Punctuation", "Identifier", "Whitespace",
+        //                                  "Eof"}[static_cast<unsigned int>(it->first)]); // used for debugging
+        out << static_cast<int>(it->first);
+
+        out << ',';
+
+        out << '(';
+
+        out << '{' << '`' << std::get<0>(it->second).first << '`' << ',' << '`' << std::get<0>(it->second).second << '`'
+            << "},";
+        out << '{' << '`' << std::get<1>(it->second).first << '`' << ',' << '`' << std::get<1>(it->second).second << '`'
+            << "},";
+        out << '`' << std::get<2>(it->second) << '`';
+
+        out << ')';
+
+        out << ((std::next(it) == v.end()) ? "}" : "},");
     }
 
     out << "}\n";
@@ -49,6 +80,32 @@ TEST_SUITE("ScannerTests") {
         while (std::get<2>(scanner.peek_next_char()) != '\0') {
             v.push_back(scanner.get_next_char());
         }
+
+        actual << v;
+
+        CHECK_EQ(expected.str(), actual.str());
+    }
+}
+
+TEST_SUITE("LexerTests") {
+    TEST_CASE("[LexerTests] factorial") {
+        Lexer lexer(Scanner(StreamReader("test/res/input/factorial.bro")));
+
+        std::ifstream expected_fstream("test/res/expected/LexerTests-factorial.txt");
+        std::stringstream expected{};
+        expected << expected_fstream.rdbuf();
+
+        std::stringstream actual{};
+
+        // std::ofstream reset("test/res/expected/LexerTests-factorial.txt");
+
+        std::vector<Lexer::NextToken> v{};
+        Lexer::NextToken next_token{};
+
+        do {
+            next_token = lexer.get_next_token();
+            v.push_back(next_token);
+        } while (next_token.first != Lexer::TokenType::Eof);
 
         actual << v;
 
