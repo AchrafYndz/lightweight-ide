ce542af8abddc5bd291ba6ba99c0a24e18e87953
exellentcoin26 <jonas.caluwe26@gmail.com>
2023-01-04 16:06:30 +0100

    added parsing of request body as json and handling both raw string source code and regular

--- core/src/main.cc
+++ core/src/main.cc
@@ -13,14 +13,16 @@
 #include <thread>
 #include <vector>
 
+#include <dbg.h>
+
 std::string generateJSON(const LR& lr, const std::string& filepath) {
     std::pair<bool, LR::ASTree*> parse_result = lr.parse(StreamReader(filepath));
 
     std::cout << "Parse result: " << ((parse_result.first) ? "success" : "failed") << '\n';
 
     // delete file from system
-    if (!std::filesystem::remove(filepath))
-        throw std::runtime_error("failed to delete temporary source file");
+     if (!std::filesystem::remove(filepath))
+         throw std::runtime_error("failed to delete temporary source file");
 
     // TODO: analyze parse tree for syntax highlighting
 
@@ -40,49 +42,20 @@ std::string generateJSON(const LR& lr, const std::string& filepath) {
 /// Heavily isnpired by (this)[https://tomeko.net/online_tools/cpp_unescape.php] tool.
 std::string raw_to_string(const std::string& code) {
     std::string result{};
-    enum class State { OPENING, STRING, ESCAPING };
-    State state{State::OPENING};
+    enum class State { Default, Escape };
+    State state{State::Default};
 
     unsigned int i{0};
     for (i = 0; i < code.length(); ++i) {
         switch (state) {
-        case State::OPENING:
-            switch (code.at(i)) {
-            case ' ':
-            case '\f':
-            case '\n':
-            case '\r':
-            case '\t':
-            case '\v':
-                break;
-            case '\"':
-                state = State::STRING;
-                break;
-            default:
-                if (i != code.length() - 1) {
-                    throw std::runtime_error(std::string() + "Unexpected character (" + code[i] +
-                                             "), expecting quotation mark. At position: " + std::to_string(i));
-                    return result;
-                } else {
-                    throw std::runtime_error(std::string() + "Unexpected character (" + code[i] +
-                                             "), conversion interrupted. At position: " + std::to_string(i));
-                    break;
-                }
-            }
-            break;
-
-        case State::STRING:
+        case State::Default:
             switch (code[i]) {
-            case '\f':
-            case '\n':
-            case '\r':
-            case '\t':
-                break;
             case '\"':
-                state = State::OPENING;
+                std::runtime_error(std::string() +
+                                   "Non escaped `\"` not allowed in string. At position: " + std::to_string(i));
                 break;
             case '\\':
-                state = State::ESCAPING;
+                state = State::Escape;
                 break;
             default:
                 result += code[i];
@@ -90,7 +63,7 @@ std::string raw_to_string(const std::string& code) {
             }
             break;
 
-        case State::ESCAPING:
+        case State::Escape:
             switch (code[i]) {
             case 'f':
                 result += '\f';
@@ -118,12 +91,12 @@ std::string raw_to_string(const std::string& code) {
                     std::to_string(i));
                 return result;
             }
-            state = State::STRING;
+            state = State::Default;
             break;
         }
     }
 
-    if (state != State::OPENING) {
+    if (state != State::Default) {
         throw std::runtime_error(std::string() +
                                  "Source string not terminated correctly. At position: " + std::to_string(i));
     }
@@ -138,6 +111,7 @@ std::string move_code_into_file(const std::string& code) {
     std::string filename = "tmp/" + (std::stringstream() << thread_id).str();
 
     std::ofstream source_file(filename);
+    // Note: `raw_to_string` is only needed when the source code is a raw string literal. It can handle both.
     source_file << raw_to_string(code);
     source_file.close();
 
@@ -153,8 +127,11 @@ int main() {
     CROW_ROUTE(app, "/")([]() { return "Hello, world!"; });
 
     CROW_ROUTE(app, "/json").methods(crow::HTTPMethod::POST)([&](const crow::request& req) {
+        // parse request body as json
+        nlohmann::json body = nlohmann::json::parse(req.body);
+
         // move source code into source file to free memory
-        const std::string filename = move_code_into_file(req.body);
+        const std::string filename = move_code_into_file(body["code"]);
 
         crow::response response(generateJSON(lr, filename));
         response.set_header("content-type", "application/json");
