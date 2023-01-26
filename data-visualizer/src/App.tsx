da2b75f3197415d21f49db15c28eab2ed7a75c9b
Florrdv <flor@ronsmans.me>
2023-01-26 18:14:06 +0100

    Switched frontend branding to c--

--- data-visualizer/src/App.tsx
+++ data-visualizer/src/App.tsx
@@ -4,7 +4,7 @@ const Header = ({ fileName }: { fileName: string }) => {
     return <>
         <div className="bg-neutral-900 p-2 pb-0 flex flex-row justify-between items-center">
             <div className="bg-neutral-600 p-1 pl-2 pr-2 cursor-pointer flex items-center justify-center gap-1 rounded-t-md">
-                <img src="./python.png" alt="Python file" className="w-3 h-3" />
+                <img src="./cmm.png" alt="Python file" className="w-3 h-3" />
                 {fileName}
             </div>
             <div className="flex flex-row gap-1 items-center justify-center pb-2">
@@ -19,7 +19,7 @@ const Header = ({ fileName }: { fileName: string }) => {
 const App = () => {
     return (
         <div className="flex flex-col w-screen h-screen text-white">
-            <Header fileName="main.pai" />
+            <Header fileName="main.cmm" />
             <Editor />
         </div>
   );
